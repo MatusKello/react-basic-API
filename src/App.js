3faa4b0840a78db7fbd3dc0f855a297812c88b38
Matus Kello <m.kello1992@gmail.com>
2023-06-22 17:47:06 +0200

    multiple returns with API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Iss from './API/Iss';
+/* import Iss from './API/Iss';
 import West from './API/West';
 
 const App = () => {
@@ -11,6 +11,43 @@ const App = () => {
         <West />
       </p>
     </div>
+  );
+};
+
+export default App; */
+
+//! new training - multiple returns
+
+import { useState, useEffect } from 'react';
+const url = 'http://api.open-notify.org/iss-now.json';
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [longitude, setLongitude] = useState('');
+  const [latitude, setLatitude] = useState('');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data['iss_position'])
+      .then((position) => {
+        setLatitude(position['latitude']);
+        setLongitude(position['longitude']);
+      });
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <h2>Loading screen</h2>;
+  }
+
+  return (
+    <>
+      <h2>Longitude</h2>
+      <p>{longitude}</p>
+      <h2>Latitude</h2>
+      <p>{latitude}</p>
+    </>
   );
 };
 
